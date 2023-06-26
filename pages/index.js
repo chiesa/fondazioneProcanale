4251386422c67cb7f89a8f088cc7d738377c64fe
chiesa <samuelechiesa.basta@gmail.com>
2023-06-26 16:38:33 +0200

    updated

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 
   return (
-    <div class="bg-[url('../public/amati1651.ico')] bg-contain  bg-no-repeat bg-center" className={styles.container} > 
-  
-      <p class='lg:text-10xl md:text-8xl text-6xl text-center text-slate-400 font-pacifico pt-40 pb-40'>Fondazione<br></br>pro Canale</p>
-
+    <div className={styles.container} > 
+      <div className="p-12 md:bg-gray-100 mt-2 mb-2 md:max-h-[73vh] md:bg-[url('../public/Rogeri.ico')] bg-[url('../public/testa.ico')] bg-contain bg-no-repeat bg-center">
+        <p class='lg:text-10xl md:text-8xl md:text-6xl text-2xl text-center text-slate-400 font-pacifico pt-40 pb-40'>Fondazione<br></br>pro Canale</p>
+      </div>
     </div>
   )
 }5
